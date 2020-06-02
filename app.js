1c80bb236038c2ed1787667dcb050bdb88d9055f
tofarati.agbaje <tofarati.agbaje@gmail.com>
2020-06-02 10:59:49 +0200

    Replaced ws server with socket io ws and p2p servers, changed broadcasting methods, removed storage, added routing for browser

--- app.js
+++ app.js
@@ -1,181 +1,50 @@
-const WebSocket = require('ws')
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+const cors = require('cors');
 
-const wss = new WebSocket.Server({ port: process.env.PORT||8000})
+const PORT = process.env.PORT||8989;
+const app = express();
+const router = express.Router();
+router.get('/', (req, res) => {
+	res.send('server is live =)');
+})
+
+var server = http.createServer(app);
+
+var p2p = require('socket.io-p2p-server').Server;
+var io = socketio(server);
+
 const DEFAULT_ROOM = '8e1286c5-407c-4ffd-bf6c-7bc621fb2f2e';
-const users = []
-const onlineUsers = []
-const clients = []
-const rooms = []
 
-const broadcast = (data, ws) => {
-	if (ws === null){
-		wss.clients.forEach((client) => {
-				client.send(JSON.stringify(data))
-		})
-		return;
-	}
-	wss.clients.forEach((client) => {
-		if (client.readyState === WebSocket.OPEN && client !== ws){
-			client.send(JSON.stringify(data))
-		}
-	})
-}
+app.use(router);
+app.use(cors());
 
-console.log('server running: '+ JSON.stringify(wss));
-wss.on('connection', (ws) => {
+server.listen(PORT, ()=>{
+	console.log("Server: "+PORT+"\n\n"+JSON.stringify(server));
+})
+
+io.use(p2p);
+
+io.on('connection', (ws) => {
 	console.log('new client...');
-	ws.on('message', (message) => {
-		const data = JSON.parse(message)
-		switch (data.type) {
-			case 'REGISTER_USR': {
-				if (!users.some( user => (user.id === data.id) && (user.name === data.name)))
-					{users.push({ id: data.id, name: data.name})}
-
-				if (onlineUsers.some( user => (user.name === data.name))){
-					ws.index = onlineUsers.findIndex(user => (user.name === data.name));
-					console.log("User: "+data.name +"\nIndex: "+ ws.index);
-					ws.send(JSON.stringify({
-						type: 'USERS',
-						onlineUsers
-					}))
-					ws.send(JSON.stringify({
-						type: 'USER_ROOMS',
-						rooms
-					}))
-				}
-				else{
-					ws.index = onlineUsers.length
-					onlineUsers.push({ id: data.id, name: data.name})
-					console.log("User: "+data.name +"\nIndex: "+ ws.index);
-					ws.send(JSON.stringify({
-						type: 'USERS',
-						onlineUsers
-					}))
-					broadcast({
-						type: 'USERS',
-						onlineUsers
-					}, ws)
-					if(rooms.some( room => room.id === DEFAULT_ROOM)){
-					let roomIndex = rooms.findIndex(room => room.id === DEFAULT_ROOM);
-					rooms[roomIndex].roomUsers.push(data.name);}
-				  else
-					{ rooms.push({id: DEFAULT_ROOM, roomName: 'General', roomUsers: [data.name]});
-					}
-					broadcast({
-						type: 'USER_ROOMS',
-						rooms
-					}, null)
-				}
-				// console.log(JSON.stringify(rooms));
-				break;
-			}
-			case 'RENAME_USR': {
-				let usrindex = users.findIndex(user => user.id === data.id)
-				let oldName = users[usrindex].name;
-				users[usrindex].name = data.name;
-
-				onlineUsers[ws.index].name = data.name;
-				let modrooms = rooms.some(room => room.Users.includes(oldName));
-				modrooms.map(room => {var i = room.roomUsers.findIndex(user => user === oldName); room.roomUsers.splice(i, 1, data.name)}
-				);
-
-				ws.send(JSON.stringify({
-					type: 'USERS',
-					onlineUsers
-				}))
-				broadcast({
-					type: 'USERS',
-					onlineUsers
-				}, ws)
-				broadcast({
-					type: 'USER_ROOMS',
-					rooms
-				}, null)
-				break;
-			}
-			case 'OUTGOING_MSG': {
-				broadcast({
-					type: 'OUTGOING_MSG',
-					id: data.id,
-					msg: data.msg,
-					sender: data.sender,
-					roomid: data.roomid
-				}, ws)
-				break;
-			}
-			case 'ADD_FEED': {
-				console.log("feed added: "+data.sender+ "\nTo:"+data.roomid);
-				broadcast({
-					type: 'ADD_FEED',
-					src: data.src,
-					sender: data.sender,
-					roomid: data.roomid
-				}, ws)
-				break;
-			}
-			case 'JOIN_ROOM': {
-				console.log("Room joined: "+data.id+ "\nBy:"+data.newUser);
-				let roomIndex = rooms.findIndex(room => room.id === data.id);
-				if (roomIndex < 0){
-					rooms[roomIndex].roomUsers.push(data.newUser);
-				} else { break; }
-				broadcast({
-					type: 'JOIN_ROOM',
-					id: data.id,
-					newUser: data.newUser
-				}, ws)
-				break;
-			}
-			case 'CREATE_ROOM': {
-				console.log("new room: "+data.roomName);
-				rooms.push({
-					id: data.id,
-					roomName: data.roomName,
-					roomUsers: data.roomUsers
-				})
-				broadcast({
-					type: 'USER_ROOMS',
-					rooms
-				}, null)
-				// broadcast({
-				// 	type: 'CREATE_ROOM',
-				// 	id: data.id,
-				// 	roomName: data.roomName,
-				// 	roomUsers: data.roomUsers
-				// }, ws)
-				break;
-			}
-			default:
-				break;
-		}
+	let clientId
+	let name
+	ws.on('hello', (data) => {
+		clientId = data.id;
+		name = data.name;
+		console.log("hello from"+name);
+		ws.broadcast.emit('peer-msg', {type: 'REGISTER_USR', id: clientId, name: data.name})
 	})
 
-	ws.on('close', () => {
-		let skip = false;
-		wss.clients.forEach((client) => {
-			if (ws.index === client.index)
-				skip = true;
-		})
-		if (!skip){
-			let usr = onlineUsers.splice(ws.index, 1);
-			console.log('we closed: '+ usr[0].name + '\nindex: '+ ws.index)
-			let genroom = rooms.find( room =>  room.id === DEFAULT_ROOM
-			)
-			if(usr[0].name){
-			genroom.roomUsers = genroom.roomUsers.filter(user => user !== usr[0].name);}
-			wss.clients.forEach((client) => {
-				if (ws.index < client.index)
-					client.index -= 1 ;
-			})
-			broadcast({
-				type: 'USERS',
-				onlineUsers
-			}, ws)
-			broadcast({
-				type: 'USER_ROOMS',
-				rooms
-			}, ws)
-		}
-		console.log('Client socket closed.')
+	ws.on('peer-msg', (data) => {
+		console.log(JSON.stringify(data));
+		ws.broadcast.emit('peer-msg', data)
+	})
+
+	ws.on('disconnect', () => {
+		console.log("bye from"+name)
+		ws.broadcast.emit('peer-msg', {type: 'REMOVE_USR', id: clientId})
+		ws.broadcast.emit('peer-msg', {type: 'RM_FROM_ROOMS', name: name})
 	})
 })
